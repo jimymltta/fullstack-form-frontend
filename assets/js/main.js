f0273bd23111fca60e63242fb63a80a951f88bd8
Jimy Marletta <jimymarletta@mbp-de-jimy.home>
2021-07-16 23:50:41 +0200

    added all files

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Page chargée");
+
+  // Envoi des données du formulaire vers le serveur
+  document
+    .querySelector("#contact-form")
+    .addEventListener("submit", async (event) => {
+      // Annulation du comportement par défaut du formulaire (refresh)
+      event.preventDefault();
+      console.log("Soumission du formulaire");
+
+      const data = {
+        firstname: document.querySelector("#firstname").value,
+        lastname: document.querySelector("#lastname").value,
+        email: document.querySelector("#email").value,
+        message: document.querySelector("#message").value,
+      };
+
+      const repose = await axios.post("http://localhost/3000/form", data);
+      console.log(response);
+
+      if (response.status === 200) {
+        alert("Merci, tout est OK");
+      } else {
+        alert("Une erreur est survenue");
+      }
+    });
+});
